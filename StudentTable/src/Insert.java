69d4858f0ea3d24d00921e10f5b581c179c52988
shuklashraddha <102161776+shuklashraddha@users.noreply.github.com>
2022-03-25 12:06:23 +0530

    Update Insert.java

--- StudentTable/src/Insert.java
+++ StudentTable/src/Insert.java
@@ -5,7 +5,7 @@ class Insert
 {
      public static void main(String[] args) throws Exception
      {
-          //read input value from keyboard
+          
           Scanner sc = new Scanner(System.in);
           System.out.println("Enter the Student No - ");
           int no = sc.nextInt();
@@ -15,7 +15,7 @@ class Insert
           String dob = sc.next();
           System.out.println("Enter the date of joining -");
           String doj = sc.next();
-          //convert String date value to java.lang.Date class
+          
           //for date of birth
           SimpleDateFormat sdf1 = new SimpleDateFormat("dd-mm-yy");
           java.util.Date udob = sdf1.parse(dob);
@@ -25,23 +25,21 @@ class Insert
           //create JDBC connection object
           Class.forName("oracle.jdbc.driver.OracleDriver");
           Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","SYSTEM","Oracle1");
-          //create prepared Statement object
           PreparedStatement ps = con.prepareStatement("insert into studentdb.student values (?,?,?,?)");
-          //set values to the parameters query
+         
           ps.setInt(1, no);
           ps.setString(2, name);
           ps.setDate(3, sqdob);
           ps.setDate(4, sqdoj);
           //execute the query
           int res = ps.executeUpdate();
-          //processing the result
           if (res == 0)
           {
                System.out.println("Record is not inserted");
           }
           else
                System.out.println("Record is inserted");
-          //close jdbc objects
+          //close jdbc 
           ps.close();
           con.close();
      }
